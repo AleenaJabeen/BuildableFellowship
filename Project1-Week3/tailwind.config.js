eb572895e8298ecac96e7c0a877230964567d13e
Wajiha Malik <aleenajabeen19648@gmail.com>
2025-09-05 17:28:05 +0500

    Project1 Week3 is added

---
+++ Project1-Week3/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+        colors:{
+            primary:"#0B1D26",
+            secondary:"#FBD784",
+            tertiary:"#FFFFFF"
+        }
+    },
+  },
+  plugins: [],
+}
